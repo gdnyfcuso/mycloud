eec21063af09e442fd3a9d37da8e0ff37d199f00
gdnyfcuso@163.com <gdnyfcuso@163.com>
2020-08-11 12:34:59 +0800

    help pro

---
+++ microservicecloud/microservicecloud-api/src/main/java/com/atguigu/springcloud/entities/Dept.java
@@ -0,0 +1,28 @@
+package com.atguigu.springcloud.entities;
+
+import java.io.Serializable;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.Accessors;
+
+
+/**
+ * @author cy
+ */
+@NoArgsConstructor//无参构造函数
+@AllArgsConstructor  //全参构造函数
+@Data//每个字段都生成get set方法
+@Accessors(chain=true)//访问链式风格
+public class Dept implements Serializable// entity --orm--- db_table
+{
+	private Long 	deptno; // 主键
+	private String 	dname; // 部门名称
+	private String 	db_source;// 来自那个数据库，因为微服务架构可以一个服务对应一个数据库，同一个信息被存储到不同数据库
+
+	
+	
+}
